7070e0cfa52fa7c59448a6053bb220ab67d281fe
HanaTaka2137 <hanataka2137@xiyang6666.top>
2026-08-25 04:48:09 +0800

    :bug: fix mismatches

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -3,7 +3,9 @@
         <div class="w-full h-full overflow-y-auto">
             <div class="h-10"></div>
             <!--  placeholder -->
-            <div class="prose prose-invert text-zinc-200" v-html="$md.render(content ?? 'Error: Unknown')"></div>
+            <ClientOnly>
+                <div class="prose prose-invert text-zinc-200" v-html="$md.render(content ?? 'Error: Unknown')"></div>
+            </ClientOnly>
         </div>
     </ContentCard>
 </template>

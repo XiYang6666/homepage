f06ade475ee41eeb181a3569775fd65477b55289
HanaTaka2137 <hanataka2137@xiyang6666.top>
2026-08-25 04:28:47 +0800

    :bug: fix social link

--- app/components/SocialList.vue
+++ app/components/SocialList.vue
@@ -3,6 +3,7 @@
         <li v-for="{ link, icon } of socials ?? []">
             <a
                 :href="link"
+                target="_blank"
                 class="flex items-center justify-center w-full h-full text-4xl text-white hover:text-slate-600 transition-colors duration-300 transform-gpu"
             >
                 <Icon :name="icon" />
